099be6b4a85958f968d9a7b1131ea8ab15febeef
satishsookun <satish.sookun@gmail.com>
2020-11-10 23:09:51 +0400

    project structure - user list - progress

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {UsersComponent} from "./container/users.component";
+import {MatRadioModule} from "@angular/material/radio";
+import {MatExpansionModule} from "@angular/material/expansion";
 
 @NgModule({
   exports: [
@@ -11,6 +13,8 @@ import {UsersComponent} from "./container/users.component";
   ],
   imports: [
     CommonModule,
+    MatRadioModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: []
